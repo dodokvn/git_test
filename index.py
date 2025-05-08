9f633268dbbdec95ec7c0c78ee41b4c773844042
dodokvn <irambodorine@gmail.com>
2025-05-08 12:09:32 +0200

    1er commit

---
+++ index.py
@@ -0,0 +1,13 @@
+username="KAYOYA"
+a="va au marché"
+print(username+' '+a)
+x=125
+y=90
+if(x>y):
+    print("x est grand")
+else:
+    print("x est petit")
+i=20
+while(i<=40):
+    print(i)
+    i=i+1
